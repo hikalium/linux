c05d7887318bbea39f9136ac755e037783572f15
hikalium <hikalium@hikalium.com>
2023-03-15 22:44:52 +0900

    update

--- hikalium/config_full_nfs_efi.sh
+++ hikalium/config_full_nfs_efi.sh
@@ -5,3 +5,6 @@
 ../scripts/config --file config_full_nfs_efi.txt --disable CONFIG_X86_DECODER_SELFTEST
 ../scripts/config --file config_full_nfs_efi.txt --disable CONFIG_MODULE_SIG
 ../scripts/config --file config_full_nfs_efi.txt --disable CONFIG_DEBUG_INFO
+
+# set CONFIG_*_TEST=n to disable all runtime tests (manually done)
+# set CONFIG_*_SELFTEST=n to disable all runtime tests (manually done)
